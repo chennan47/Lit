50e9d2c00651945adbcb1a4cea29aee23b421f75
chennan47 <nc6r@virginia.edu>
2018-03-19 00:28:13 -0400

    basic calculator

--- weightedrandom.py
+++ weightedrandom.py
@@ -1,3 +1,12 @@
+# /**
+# * Design a data structure to hold objects with a corresponding integer weight. It should support:
+# Obtain an object randomly with probability equal to
+# (weight of the element) / (sum of the weights).
+# Set an object-weight pair. If the object is already in the structure, its weight will
+# be updated. Otherwise, the object will be inserted and set to its weight. If the weight is zero, the object can be removed.
+# put("A",3) 3/33 put("B",30) 30/33 *
+# */
+
 import random
 
 
