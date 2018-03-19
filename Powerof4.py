716bdb15b1001e0d4096be1f785b4babe5111c98
chennan47 <nc6r@virginia.edu>
2018-03-18 23:46:01 -0400

    two more

---
+++ Powerof4.py
@@ -0,0 +1,27 @@
+# 1. A simple method is to take log of the given number on base 4, and if we get an integer then number is power of 4.
+#
+# 2. Another solution is to keep dividing the number by 4, i.e, do n = n/4 iteratively.
+# In any iteration, if n%4 becomes non-zero and n is not 1 then n is not a power of 4, otherwise n is a power of 4.
+# Python3 program to check if given
+# number is power of 4 or not
+
+# Function to check if x is power of 4
+def isPowerOfFour(n):
+    if (n == 0):
+        return False
+    while (n != 1):
+        if (n % 4 != 0):
+            return False
+        n = n // 4
+
+    return True
+
+
+# Driver code
+test_no = 64
+if (isPowerOfFour(64)):
+    print(test_no, 'is a power of 4')
+else:
+    print(test_no, 'is not a power of 4')
+
+    # This code is contributed by Danish Raza
---
+++ RunningMedian.py
@@ -0,0 +1,147 @@
+#295. Find Median from Data Stream
+# Median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value.
+# So the median is the mean of the two middle value.
+#
+# Examples:
+# [2,3,4] , the median is 3
+#
+# [2,3], the median is (2 + 3) / 2 = 2.5
+#
+# Design a data structure that supports the following two operations:
+#
+# void addNum(int num) - Add a integer number from the data stream to the data structure.
+# double findMedian() - Return the median of all elements so far.
+# For example:
+#
+# addNum(1)
+# addNum(2)
+# findMedian() -> 1.5
+# addNum(3)
+# findMedian() -> 2
+
+# I keep two heaps (or priority queues):
+#
+# Max-heap small has the smaller half of the numbers.
+# Min-heap large has the larger half of the numbers.
+#
+# This gives me direct access to the one or two middle values (they’re the tops of the heaps),
+# so getting the median takes O(1) time. And adding a number takes O(log n) time.
+#
+# Supporting both min- and max-heap is more or less cumbersome, depending on the language,
+# so I simply negate the numbers in the heap in which I want the reverse of the default order.
+# To prevent this from causing a bug with -231 (which negated is itself, when using 32-bit ints),
+# I use integer types larger than 32 bits.
+#
+# Using larger integer types also prevents an overflow error when taking the mean of the two middle numbers.
+# I think almost all solutions posted previously have that bug.
+
+
+from heapq import *
+
+
+class MedianFinder:
+
+    def __init__(self):
+        self.heaps = [], []
+
+    def addNum(self, num):
+        small, large = self.heaps
+        heappush(small, -heappushpop(large, num))
+        if len(large) < len(small):
+            heappush(large, -heappop(small))
+
+    def findMedian(self):
+        small, large = self.heaps
+        if len(large) > len(small):
+            return float(large[0])
+        return (large[0] - small[0]) / 2.0
+
+import math
+
+
+#Average and Standard Deviation:
+class RollingStatistic(object):
+
+    def __init__(self, window_size, average, variance):
+        self.N = window_size
+        self.average = average
+        self.variance = variance
+        self.stddev = math.sqrt(self.variance)
+
+    def update(self, new, old):
+        oldavg = self.average
+        newavg = oldavg + (new - old)/self.N
+        self.average = newavg
+        self.variance += (new-old)*(new-newavg+old-oldavg)/(self.N-1)
+        self.stddev = math.sqrt(self.variance)
+
+# public class RunningStatDemo {
+#
+#   public static void main(String[] args) {
+#
+#     RunningStatDemo rsd = new RunningStatDemo();
+#     rsd.go();
+#   }
+#
+#   private void go() {
+#
+#     RunningStat rs = new RunningStat();
+#     rs.put(1);
+#     System.out.println("ave: " + rs.getAverage());
+#     System.out.println("std: " + rs.getStandardDeviation());
+#
+#     rs.put(1);
+#     System.out.println("ave: " + rs.getAverage());
+#     System.out.println("std: " + rs.getStandardDeviation());
+#
+#     rs.put(10);
+#     System.out.println("ave: " + rs.getAverage());
+#     System.out.println("std: " + rs.getStandardDeviation());
+#
+#     rs.put(20);
+#     System.out.println("ave: " + rs.getAverage());
+#     System.out.println("std: " + rs.getStandardDeviation());
+#
+#     rs.put(50);
+#     System.out.println("ave: " + rs.getAverage());
+#     System.out.println("std: " + rs.getStandardDeviation());
+#
+#     rs.put(50);
+#     System.out.println("ave: " + rs.getAverage());
+#     System.out.println("std: " + rs.getStandardDeviation());
+#   }
+#
+#   public class RunningStat {
+#
+#     private int count = 0;
+#     private double average = 0.0;
+#     private double pwrSumAvg = 0.0;
+#     private double stdDev = 0.0;
+#
+#     /**
+#      * Incoming new values used to calculate the running statistics
+#      *
+#      * @param value
+#      */
+#     public void put(double value) {
+#
+#       count++;
+#       average += (value - average) / count;
+#       pwrSumAvg += (value * value - pwrSumAvg) / count;
+#       stdDev = Math.sqrt((pwrSumAvg * count - count * average * average) / (count - 1));
+#
+#     }
+#
+#     public double getAverage() {
+#
+#       return average;
+#     }
+#
+#     public double getStandardDeviation() {
+#
+#       return Double.isNaN(stdDev) ? 0.0 : stdDev;
+#     }
+#
+#   }
+#
+# }
